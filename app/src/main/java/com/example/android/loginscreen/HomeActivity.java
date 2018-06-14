34bf8cabe57a51c467e3eee3505041de68adae25
bernardo.castelo <bernardo.castelo@bee-eng.pt>
2018-06-14 10:27:19 +0100

    Fragment home

--- app/src/main/java/com/example/android/loginscreen/HomeActivity.java
+++ app/src/main/java/com/example/android/loginscreen/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.android.loginscreen;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -38,18 +42,54 @@ public class HomeActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-
         /** Centrar o texto de cada item do Navigation Drawer */
         for (int positionOfMenuItem = 0; positionOfMenuItem < navigationView.getMenu().size(); positionOfMenuItem++) {
             MenuItem item = navigationView.getMenu().getItem(positionOfMenuItem);
-
             SpannableString s = new SpannableString(item.getTitle().toString());
             s.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_CENTER), 0, s.length(), 0);
-
             item.setTitle(s);
         }
 
+        /** Instancia o fragmento correspondente à opção do menu **/
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                Class fragmentClass = null;
+                /** Selecionar que fragmento criar com base ao menu escolhido **/
+                switch (item.getItemId()){
+                    case R.id.nav_item_home:
+                        fragmentClass = HomePageFragment.class;
+                        break;
+                    case R.id.nav_item_horario:
+                        //menu2
+                        break;
+                    case R.id.nav_item_pedidostroca:
+                        //menu3
+                        break;
+                }
+                /** instanciar o fragmento **/
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
+                /** colocar o fragmento no container da pagina principal **/
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+
+                /** mudar a cor do texto do menu e fechar o menu **/
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean moveDatabaseFrom(Context sourceContext, String name) {
+        return super.moveDatabaseFrom(sourceContext, name);
     }
 
     private void initialize(){
