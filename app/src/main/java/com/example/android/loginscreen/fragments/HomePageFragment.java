8ee16c0ead4b8e068985e6660fd1b530432451a0
Pedromrl <pedromrlemos@gmail.com>
2018-06-15 16:07:51 +0100

    RecyclerView do home page fragment finalizada

--- app/src/main/java/com/example/android/loginscreen/fragments/HomePageFragment.java
+++ app/src/main/java/com/example/android/loginscreen/fragments/HomePageFragment.java
@@ -2,17 +2,46 @@ package com.example.android.loginscreen.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.android.loginscreen.R;
+import com.example.android.loginscreen.adapters.AprovesAdapter;
+import com.example.android.loginscreen.models.Aprove;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePageFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view_home_page);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext(),1);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<Aprove> listaDeAproves = new ArrayList<Aprove>();
+        for (int i = 0; i < 6; i++){
+            String userFoto = "https://images.unsplash.com/photo-1520272820796-02e71f701951?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9810af68e5144e9a7ad27f6298e86587&auto=format&fit=crop&w=634&q=80";
+            String userName = "Pedro " + i;
+            String turno = "Turno da manhã " + i;
+            String data = "26 de Maio " + i;
+            String preferencia = "Tarde " + i;
+            listaDeAproves.add(new Aprove(userFoto, userName, turno, data, preferencia));
+        }
+
+        recyclerView.setAdapter(new AprovesAdapter(listaDeAproves, R.layout.espera_aprovacao_card, getContext()));
+
+
+        return view;
     }
 }
